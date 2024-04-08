a209a566922802f787ab2fd0d88d8f6b403a55f8
Brady Kelly <brady@bradykelly.net>
2024-04-09 01:53:17 +0200

    Top row sorted after ditching tw

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import analogjsangular from "@analogjs/astro-angular";
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [analogjsangular(), tailwind()]
+  integrations: [analogjsangular()]
 });
