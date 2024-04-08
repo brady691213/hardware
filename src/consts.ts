36750c5191a5d633e6c632fc2c9415f73e16a8e9
Brady Kelly <brady@bradykelly.net>
2024-04-08 12:48:25 +0200

    Going mad with Tailwind

--- src/consts.ts
+++
@@ -1,3 +0,0 @@
-const businessName = "Whitfield Hardware"
-const contactNumber = "+27 11 123-4567"
-const contactEmail = "whitfield.hardware@hardware.com"
---
+++ src/consts/business.ts
@@ -0,0 +1,6 @@
+const businessName = "Whitfield Hardware"
+const contactNumber = "+27 11 123-4567"
+const contactEmail = "whitfield@hardware.com"
+const logoAlt = "Logo Ipsum"
+
+const orangeColor = "#F18333"
